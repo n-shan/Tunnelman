7431bef37eab15b2d776e3ce714e56682d8fe79f
n-shan <47575072+n-shan@users.noreply.github.com>
2019-12-01 18:26:54 -0800

    Add Earth and Actor class

--- Actor.h
+++ Actor.h
@@ -4,5 +4,17 @@
 #include "GraphObject.h"
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
-
+class Actor : public GraphObject{
+public:
+    
+private:
+    
+};
 #endif // ACTOR_H_
+
+class Earth : public Actor {
+public:
+    
+private:
+    
+};
