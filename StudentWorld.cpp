2385e02cabf0f3260c966a547f64130c3038c96a
n-shan <47575072+n-shan@users.noreply.github.com>
2019-12-02 18:43:11 -0800

    Add vector of actors and clean up

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -2,9 +2,36 @@
 #include <string>
 using namespace std;
 
-GameWorld* createStudentWorld(string assetDir)
-{
+// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+
+GameWorld* createStudentWorld(string assetDir) {
 	return new StudentWorld(assetDir);
 }
 
-// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+int StudentWorld::init() {
+    //display earth grid
+    for(int i = 0; i < 60; i++) {
+        for(int j = 0; j < 60; j++) {
+            //create initial tunnel
+            if(i < 30 || i > 33 || j < 4)
+                earthGrid[i][j].reset(new Earth(i, j));
+        }
+    }
+    //display tunnelman
+    tunnelMan.reset(new Tunnelman(this));
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+int StudentWorld::move() {
+    // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+    decLives();
+    //return GWSTATUS_PLAYER_DIED;
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+void StudentWorld::cleanUp() {
+    
+}
+
+
--- StudentWorld.h
+++ StudentWorld.h
@@ -6,48 +6,22 @@
 #include "Actor.h"
 #include <string>
 #include <memory>
+#include <vector>
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
-class StudentWorld : public GameWorld
-{
+class StudentWorld : public GameWorld {
 public:
-	StudentWorld(std::string assetDir)
-		: GameWorld(assetDir)
-	{
-	}
-
-	virtual int init()
-	{
-        //display earth grid
-        for(int i = 0; i < 60; i++) {
-            for(int j = 0; j < 60; j++) {
-                //create initial tunnel
-                if(i < 30 || i > 33 || j < 4)
-                    earthGrid[i][j].reset(new Earth(i, j));
-            }
-        }
-        //display tunnelman
-        tMan.reset(new Tunnelman);
-		return GWSTATUS_CONTINUE_GAME;
-	}
-
-	virtual int move()
-	{
-		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		decLives();
-		//return GWSTATUS_PLAYER_DIED;
-        return GWSTATUS_CONTINUE_GAME;
-	}
-
-	virtual void cleanUp()
-	{
-	}
-
+	StudentWorld(std::string assetDir) : GameWorld(assetDir) { }
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+    
 private:
     std::unique_ptr<Earth> earthGrid[60][60];
-    std::unique_ptr<Tunnelman> tMan;
+    std::unique_ptr<Tunnelman> tunnelMan;
+    //all actors
+    std::vector<std::unique_ptr<Actor>> actors;
 };
 
 #endif // STUDENTWORLD_H_
